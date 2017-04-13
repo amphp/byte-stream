39584ab47ea83f5416cbec6ba1d8802b7e1f4c6a
Aaron Piotrowski <aaron@trowski.com>
2017-04-13 18:14:27 -0500

    Remove specific success value from write() and end()

--- lib/WritableStream.php
+++ lib/WritableStream.php
@@ -8,14 +8,14 @@ interface WritableStream {
     /**
      * @param string $data
      *
-     * @return \Amp\Promise<int>
+     * @return \Amp\Promise Succeeds once the data has been successfully written to the stream.
      */
     public function write(string $data): Promise;
     
     /**
      * @param string $data
      *
-     * @return \Amp\Promise<int>
+     * @return \Amp\Promise Succeeds once the data has been successfully written to the stream and the stream ended.
      */
-    public function end(string $data = ''): Promise;
+    public function end(string $data = null): Promise;
 }
