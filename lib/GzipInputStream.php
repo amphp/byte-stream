23f5ba91b96773afed87766de5306d22715e99bd
Niklas Keller <me@kelunik.com>
2017-05-07 11:14:41 +0200

    Simplify GzipInputStream by removing the state tracking

--- lib/GzipInputStream.php
+++ lib/GzipInputStream.php
@@ -6,14 +6,8 @@ use Amp\Promise;
 use function Amp\call;
 
 class GzipInputStream implements InputStream {
-    const STATE_FAILED = -1;
-    const STATE_NORMAL = 0;
-    const STATE_ENDING = 1;
-    const STATE_ENDED = 2;
-
     private $source;
     private $resource;
-    private $state = 0;
 
     public function __construct(InputStream $source) {
         $this->source = $source;
@@ -26,30 +20,20 @@ class GzipInputStream implements InputStream {
 
     public function read(): Promise {
         return call(function () {
-            if ($this->state === self::STATE_ENDED) {
-                throw new ClosedException("The stream has already been closed");
-            }
-
-            if ($this->state === self::STATE_ENDING) {
-                $this->state = self::STATE_ENDED;
-                return null;
-            }
-
-            if ($this->state === self::STATE_FAILED) {
-                throw new StreamException("The stream has previously failed");
-            }
-
             $data = yield $this->source->read();
 
             if ($data === null) {
+                if ($this->resource === null) {
+                    return null;
+                }
+
                 $decompressed = \inflate_add($this->resource, "", \ZLIB_FINISH);
 
-                $this->state = self::STATE_ENDING;
-
                 if ($decompressed === false) {
-                    $this->state = self::STATE_FAILED;
                     throw new StreamException("Failed adding data to deflate context");
                 }
+
+                $this->resource = null;
 
                 return $decompressed;
             }
@@ -57,7 +41,6 @@ class GzipInputStream implements InputStream {
             $decompressed = \inflate_add($this->resource, $data, \ZLIB_SYNC_FLUSH);
 
             if ($decompressed === false) {
-                $this->state = self::STATE_FAILED;
                 throw new StreamException("Failed adding data to deflate context");
             }
 
@@ -66,7 +49,8 @@ class GzipInputStream implements InputStream {
     }
 
     public function close() {
-        $this->state = self::STATE_ENDED;
+        $this->resource = null;
+
         $this->source->close();
     }
 }
