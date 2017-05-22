6b369657a13cd675c5d1c4a592b7d83fa118857e
Niklas Keller <me@kelunik.com>
2017-05-22 10:41:35 +0200

    Reject non-strings in IteratorStream

--- lib/IteratorStream.php
+++ lib/IteratorStream.php
@@ -3,11 +3,13 @@
 namespace Amp\ByteStream;
 
 use Amp\Deferred;
+use Amp\Failure;
 use Amp\Iterator;
 use Amp\Promise;
 
 class IteratorStream implements InputStream {
     private $iterator;
+    private $exception;
 
     public function __construct(Iterator $iterator) {
         $this->iterator = $iterator;
@@ -15,13 +17,31 @@ class IteratorStream implements InputStream {
 
     /** @inheritdoc */
     public function read(): Promise {
+        if ($this->exception) {
+            return new Failure($this->exception);
+        }
+
         $deferred = new Deferred;
 
         $this->iterator->advance()->onResolve(function ($error, $hasNextElement) use ($deferred) {
             if ($error) {
+                $this->exception = $error;
                 $deferred->fail($error);
             } elseif ($hasNextElement) {
-                $deferred->resolve($this->iterator->getCurrent());
+                $chunk = $this->iterator->getCurrent();
+
+                if (!\is_string($chunk)) {
+                    $this->exception = new StreamException(\sprintf(
+                        "Unexpected iterator value of type '%s', expected string",
+                        \is_object($chunk) ? \get_class($chunk) : \gettype($chunk)
+                    ));
+
+                    $deferred->fail($this->exception);
+
+                    return;
+                }
+
+                $deferred->resolve($chunk);
             } else {
                 $deferred->resolve(null);
             }
