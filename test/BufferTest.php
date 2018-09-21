f7da6d0ad39de9a5a91943bf6641c536456042f3
Niklas Keller <me@kelunik.com>
2018-09-21 22:27:35 +0200

    Add buffer test

---
+++ test/BufferTest.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Amp\ByteStream\Test;
+
+use function Amp\ByteStream\buffer;
+use Amp\ByteStream\IteratorStream;
+use Amp\Iterator;
+use Amp\PHPUnit\TestCase;
+use function Amp\Promise\wait;
+
+class BufferTest extends TestCase
+{
+    public function testBuffer()
+    {
+        $stream = new IteratorStream(Iterator\fromIterable(["abc", "def", "g"], 10));
+
+        $this->assertSame("abcdefg", wait(buffer($stream)));
+    }
+}
