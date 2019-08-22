076c05714a1204f0fcce948c23e5f66195211c3e
Niklas Keller <me@kelunik.com>
2019-08-22 22:34:18 +0200

    Remove void return type for PHP 7.0 compat

--- lib/LineReader.php
+++ lib/LineReader.php
@@ -55,7 +55,7 @@ final class LineReader
         return $this->buffer;
     }
 
-    public function clearBuffer(): void
+    public function clearBuffer()
     {
         $this->buffer = "";
     }
