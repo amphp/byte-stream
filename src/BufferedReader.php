dfa6ef39942a86225ff3c385acbe798a36dbf4a1
Aaron Piotrowski <aaron@trowski.com>
2021-12-30 19:43:28 -0600

    Add BufferedReader

---
+++ src/BufferedReader.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Amp\ByteStream;
+
+use Amp\Cancellation;
+
+final class BufferedReader
+{
+    private string $buffer = '';
+
+    private bool $pending = false;
+
+    public function __construct(
+        private ReadableStream $stream,
+    ) {
+    }
+
+    public function getBuffer(): string
+    {
+        if ($this->pending) {
+            throw new PendingReadError('Cannot retrieve the buffer while a read operation is pending');
+        }
+
+        $buffer = $this->buffer;
+        $this->buffer = '';
+        return $buffer;
+    }
+
+    /**
+     * @template TString as string|null
+     *
+     * @param \Closure():TString $read
+     */
+    private function guard(\Closure $read): mixed
+    {
+        if ($this->pending) {
+            throw new PendingReadError();
+        }
+
+        $this->pending = true;
+
+        try {
+            return $read();
+        } finally {
+            $this->pending = false;
+        }
+    }
+
+    /**
+     * @throws StreamException If the implementation of {@see ReadableStream::read()} of the instance given the
+     * constructor can throw.
+     *
+     * @see ReadableStream::read() Identical to this method, returning data from the internal buffer first.
+     */
+    public function read(?Cancellation $cancellation = null): ?string
+    {
+        return $this->guard(function () use ($cancellation): ?string {
+            if ($this->buffer !== '') {
+                $buffer = $this->buffer;
+                $this->buffer = '';
+                return $buffer;
+            }
+
+            return $this->stream->read($cancellation);
+        });
+    }
+
+    /**
+     * @param positive-int $length The number of bytes to read from the stream. Fewer bytes may be returned if the
+     * stream closes before the given number of bytes can be read.
+     *
+     * @throws StreamException If the implementation of {@see ReadableStream::read()} of the instance given the
+     * constructor can throw.
+     */
+    public function readLength(int $length, ?Cancellation $cancellation = null): string
+    {
+        if ($length <= 0) {
+            throw new \ValueError('The number of bytes to read must be a positive integer');
+        }
+
+        return $this->guard(function () use ($length, $cancellation): string {
+            while (\strlen($this->buffer) < $length) {
+                $chunk = $this->stream->read($cancellation);
+                if ($chunk === null) {
+                    $buffer = $this->buffer;
+                    $this->buffer = '';
+                    return $buffer;
+                }
+                $this->buffer .= $chunk;
+            }
+
+            $buffer = \substr($this->buffer, 0, $length);
+            $this->buffer = \substr($this->buffer, $length);
+            return $buffer;
+        });
+    }
+
+    /**
+     * @param non-empty-string $suffix Read from the stream until the given suffix is found in the stream, at which
+     * point all bytes up to and including the suffix will be returned. If the stream closes before the suffix is found,
+     * the bytes read up to that point will be returned.
+     *
+     * @throws StreamException If the implementation of {@see ReadableStream::read()} of the instance given the
+     * constructor can throw.
+     */
+    public function readUntil(string $suffix, ?Cancellation $cancellation = null): string
+    {
+        $length = \strlen($suffix);
+
+        if (!$length) {
+            throw new \ValueError('The suffix must be a non-empty string');
+        }
+
+        return $this->guard(function () use ($suffix, $length, $cancellation): string {
+            while (($position = \strpos($this->buffer, $suffix)) === false) {
+                $chunk = $this->stream->read($cancellation);
+                if ($chunk === null) {
+                    $buffer = $this->buffer;
+                    $this->buffer = '';
+                    return $buffer;
+                }
+                $this->buffer .= $chunk;
+            }
+
+            /** @psalm-suppress PossiblyUndefinedVariable */
+            $position += $length;
+            $buffer = \substr($this->buffer, 0, $position);
+            $this->buffer = \substr($this->buffer, $position);
+            return $buffer;
+        });
+    }
+
+    /**
+     * @see buffer()
+     *
+     * @throws BufferException If the $limit given is exceeded.
+     * @throws StreamException If the implementation of {@see ReadableStream::read()} of the instance given the
+     * constructor can throw.
+     */
+    public function buffer(?Cancellation $cancellation = null, int $limit = \PHP_INT_MAX): string
+    {
+        return $this->guard(function () use ($cancellation, $limit): string {
+            $length = \strlen($this->buffer);
+            $chunks = $this->buffer === '' ? [] : [$this->buffer];
+            $this->buffer = '';
+
+            while (null !== $chunk = $this->stream->read($cancellation)) {
+                $chunks[] = $chunk;
+                $length += \strlen($chunk);
+                if ($length > $limit) {
+                    throw new BufferException(\implode($chunks), "Max length of $limit bytes exceeded");
+                }
+
+                unset($chunk); // free memory
+            }
+
+            return \implode($chunks);
+        });
+    }
+}
---
+++ test/BufferedReaderTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Amp\ByteStream;
+
+use Amp\PHPUnit\AsyncTestCase;
+use Amp\Pipeline\Pipeline;
+
+final class BufferedReaderTest extends AsyncTestCase
+{
+    private const CHUNK = "Buffered String\n";
+    private const REPEAT_COUNT = 10;
+
+    private BufferedReader $bufferedReader;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $pipeline = Pipeline::concat([
+            Pipeline::fromIterable(\array_fill(0, self::REPEAT_COUNT, self::CHUNK . self::CHUNK))
+                ->delay(0.01),
+            Pipeline::fromIterable([\trim(self::CHUNK)]),
+        ]);
+
+        $this->bufferedReader = new BufferedReader(new IterableStream($pipeline));
+    }
+
+    public function testRead(): void
+    {
+        $length = \strlen(self::CHUNK);
+
+        for ($i = 0; $i < self::REPEAT_COUNT; ++$i) {
+            self::assertSame(self::CHUNK, $this->bufferedReader->readLength($length));
+            self::assertSame(self::CHUNK, $this->bufferedReader->read());
+        }
+
+        self::assertSame(\trim(self::CHUNK), $this->bufferedReader->read());
+    }
+
+    public function testReadUntil(): void
+    {
+        for ($i = 0; $i < self::REPEAT_COUNT * 2; ++$i) {
+            self::assertSame(self::CHUNK, $this->bufferedReader->readUntil("\n"));
+        }
+
+        self::assertSame(\trim(self::CHUNK), $this->bufferedReader->readUntil("\n"));
+    }
+
+    public function testReadLength(): void
+    {
+        $length = \strlen(self::CHUNK);
+
+        for ($i = 0; $i < self::REPEAT_COUNT * 2; ++$i) {
+            self::assertSame(self::CHUNK, $this->bufferedReader->readLength($length));
+        }
+
+        self::assertSame(\trim(self::CHUNK), $this->bufferedReader->readLength($length));
+    }
+
+    public function testReadingBytes(): void
+    {
+        $length = \strlen(self::CHUNK);
+
+        for ($i = 0; '' !== $byte = $this->bufferedReader->readLength(1); ++$i) {
+            self::assertSame(self::CHUNK[$i % $length], $byte);
+        }
+    }
+}
