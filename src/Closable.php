385877c7bcbc719b1311bdb2749b317dd07ab394
Aaron Piotrowski <aaron@trowski.com>
2022-04-03 11:40:21 -0500

    Deprecate Closable
    The interface has been moved to amphp/amp.

--- src/Closable.php
+++ src/Closable.php
@@ -2,30 +2,11 @@
 
 namespace Amp\ByteStream;
 
-interface Closable
+use Amp\Closable as AmpClosable;
+
+/**
+ * @deprecated Use {@see AmpClosable} instead of this interface. This interface will be removed in a future release.
+ */
+interface Closable extends AmpClosable
 {
-    /**
-     * Closes the stream or resource, marking it as unreadable and/or unwritable.
-     *
-     * - Whether pending reads are aborted or not is implementation dependent.
-     * - New read operations should immediately return {@code null}.
-     *
-     * - Whether pending writes are aborted or not is implementation dependent.
-     * - New write operations should throw.
-     */
-    public function close(): void;
-
-    /**
-     * Returns whether this resource has been closed.
-     *
-     * @return bool {@code true} if closed, otherwise {@code false}
-     */
-    public function isClosed(): bool;
-
-    /**
-     * Registers a callback that is invoked when this resource is closed.
-     *
-     * @param \Closure():void $onClose
-     */
-    public function onClose(\Closure $onClose): void;
 }
